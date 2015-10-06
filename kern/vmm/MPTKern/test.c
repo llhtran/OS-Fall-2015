39d26d04ab73e0b3e7592c241a8c3899086af46e
Lien Tran <lien.tran@yale.edu>
2015-10-06 05:55:31 -0400

    debuggin Kern

--- kern/vmm/MPTKern/test.c
+++ kern/vmm/MPTKern/test.c
@@ -19,11 +19,11 @@ int MPTKern_test1()
   if (get_ptbl_entry_by_va(1, vaddr) == 0) {
     dprintf("test 1 failed.\n");
     return 1;
-  }/*
+  }
   if (get_pdir_entry_by_va(1, vaddr) == 0) {
     dprintf("test 1 failed.\n");
     return 1;
-  }
+  }/*
   unmap_page(1, vaddr);
   if (get_ptbl_entry_by_va(1, vaddr) != 0) {
     dprintf("test 1 failed.\n");
