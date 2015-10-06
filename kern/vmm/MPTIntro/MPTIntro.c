1d58915ac44b5a47ce632b29e7b618c9e807092a
Lien Tran <lien.tran@yale.edu>
2015-10-05 22:14:53 -0400

    Intro and Op tentatively done

--- kern/vmm/MPTIntro/MPTIntro.c
+++ kern/vmm/MPTIntro/MPTIntro.c
@@ -69,7 +69,8 @@ unsigned int get_ptbl_entry(unsigned int proc_index, unsigned int pde_index, uns
 {   
     // eliminating the permission bits
     unsigned int pte = (unsigned int) PDirPool[proc_index][pde_index];
-    unsigned int * addr = (unsigned int *) pte & (unsigned int) (~(0) << 3);
+    unsigned int temp = pte & (unsigned int) (~(0) << 3);
+    unsigned int * addr = temp;
     return addr[pte_index];
 }
 
@@ -78,7 +79,8 @@ unsigned int get_ptbl_entry(unsigned int proc_index, unsigned int pde_index, uns
 void set_ptbl_entry(unsigned int proc_index, unsigned int pde_index, unsigned int pte_index, unsigned int page_index, unsigned int perm)
 {   
     unsigned int pte = (unsigned int) PDirPool[proc_index][pde_index];
-    unsigned int * addr = (unsigned int *) pte & (unsigned int) (~(0) << 3);
+    unsigned int temp = pte & (unsigned int) (~(0) << 3);
+    unsigned int * addr = temp;
     addr[pte_index] = (unsigned int) PAGESIZE * page_index + perm; 
 }   
 
@@ -94,6 +96,7 @@ void set_ptbl_entry_identity(unsigned int pde_index, unsigned int pte_index, uns
 void rmv_ptbl_entry(unsigned int proc_index, unsigned int pde_index, unsigned int pte_index)
 {
     unsigned int pte = (unsigned int) PDirPool[proc_index][pde_index];
-    unsigned int * addr = (unsigned int *) pte & (unsigned int) (~(0) << 3);
+    unsigned int temp = pte & (unsigned int) (~(0) << 3);
+    unsigned int * addr = temp;
     addr[pte_index] = (unsigned int) 0;
 }
